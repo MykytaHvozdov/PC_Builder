1e6a918019f2f8858aaba21b6a7ac6b7310d29e6
Kapchuk-Yuriy <yurii.kapchuk@lnu.edu.ua>
2024-11-30 00:26:48 +0200

    Authism

--- Client/src/app/services/auth.service.ts
+++ Client/src/app/services/auth.service.ts
@@ -4,6 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private _isAuthorized: boolean = false;
 
-  constructor() { }
+  get isAuthorized(): boolean {
+    return this._isAuthorized;
+  }
+
+  login(isAdmin: boolean, userId: number): void {
+    this._isAuthorized = true;
+
+    localStorage.setItem('isAuthorized', 'true');
+  }
+
+  logout(): void {
+    this._isAuthorized = false;
+
+    localStorage.removeItem('isAuthorized');
+  }
+
+  constructor() {
+    const isAuthorized = localStorage.getItem('isAuthorized');
+    if (isAuthorized === 'true') {
+        this._isAuthorized = true;
+    }
+  }
 }
